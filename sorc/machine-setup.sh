14f122aaec958b98eb455a40933602a211be0ae2
David Huber <david.huber@noaa.gov>
2025-06-24 12:53:22 -0500

    Port to Ursa

--- sorc/machine-setup.sh
+++ sorc/machine-setup.sh
@@ -27,13 +27,14 @@ if [[ -d /lfs5 ]] ; then
     fi
     target=jet
     module purge
-elif [[ -d /scratch1/NCEPDEV ]] ; then
-    # We are on NOAA Hera
-    if ( ! eval module help > /dev/null 2>&1 ) ; then
-        echo load the module command 1>&2
-        source /apps/lmod/lmod/init/$__ms_shell
+elif [[ -d /scratch3/NCEPDEV ]] ; then
+    # We are on NOAA Hera or Ursa
+    mount=$(findmnt -n -o SOURCE /home)
+    if [[ ${mount} =~ "ursa" ]]; then
+      target=ursa
+    else
+      target=hera
     fi
-    target=hera
     module purge
 elif [[ -d /work2/noaa ]]; then
   # We are on MSU Orion or Hercules
