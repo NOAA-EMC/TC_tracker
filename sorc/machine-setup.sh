8bd0198a0060ec76742e9be0d2e0afdfd0e8a168
David Huber <david.huber@noaa.gov>
2025-06-24 08:47:56 -0500

    Fix MSU target identification

--- sorc/machine-setup.sh
+++ sorc/machine-setup.sh
@@ -39,16 +39,11 @@ elif [[ -d /work2/noaa ]]; then
   # We are on MSU Orion or Hercules
   mount=$(findmnt -n -o SOURCE /home)
   if [[ ${mount} =~ "hercules" ]]; then
-    MACHINE_ID=hercules
+    target=hercules
   else
-    MACHINE_ID=orion
+    target=orion
   fi
-    target=orion || hercules
     module purge
-    module use /apps/modulefiles/core
-    module use /apps/contrib/modulefiles
-    module use /apps/contrib/NCEPLIBS/lib/modulefiles
-    module use /apps/contrib/NCEPLIBS/orion/modulefiles
 elif [[ -d /gpfs/f5 ]]; then
   # We are on GAEAC5.
   target=gaeac5
