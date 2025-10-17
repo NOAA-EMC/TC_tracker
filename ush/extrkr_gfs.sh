4a1b38c004be9f37607b7811b258a49d605768b4
AntonMFernando-NOAA <167725623+AntonMFernando-NOAA@users.noreply.github.com>
2025-10-17 13:08:11 -0400

    Rename Atm Files to Standardized Form (#20)
    * update ush/data_check_gfs.sh
    
    * update extrkr scripts

--- ush/extrkr_gfs.sh
+++ ush/extrkr_gfs.sh
@@ -208,7 +208,7 @@ case ${cmodel} in
        echo " "; set -x                                    ;
 #       gfsdir=${gfsdir:-${COMINgfs:?}/${cyc}}                     ;
        gfsdir=${gfsdir:-${COMINgfs:?}/${cyc}/${COMPONENT}} ;
-       gfsgfile=gfs.t${cyc}z.pgrb2.0p25.f                  ;
+       gfsgfile=gfs.t${cyc}z.pres_a.0p25.f                  ;
 
        vit_incr=${FHOUT_CYCLONE:-6}                        ;
        fcstlen=${FHMAX_CYCLONE:-240}                       ;
@@ -1294,18 +1294,18 @@ then
     for fhour in ${fcsthrs}
     do
   
-      if [ ! -s ${gfsdir:?}/${gfsgfile}${fhour} ]
+      if [ ! -s ${gfsdir:?}/${gfsgfile}${fhour}.grib2 ]
       then
         set +x
         echo " "
-        echo "FATAL ERROR:  GFS File missing: ${gfsdir}/${gfsgfile}${fhour}"
+        echo "FATAL ERROR:  GFS File missing: ${gfsdir}/${gfsgfile}${fhour}.grib2"
         echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
         echo " "
         set -x
-        err_exit "MISSING GFS FILE IN extrkr_gfs.sh: ${gfsdir}/${gfsgfile}${fhour}"
+        err_exit "MISSING GFS FILE IN extrkr_gfs.sh: ${gfsdir}/${gfsgfile}${fhour}.grib2"
       fi
 
-      gfile=${gfsdir}/${gfsgfile}${fhour}
+      gfile=${gfsdir}/${gfsgfile}${fhour}.grib2
       ${WGRIB2:?} $gfile -match "$PARMlist" -grib ${TRKDATA}/master.gfsgribfile.${PDY}${cyc}.f${fhour}
 
       gfs_master_file=${TRKDATA}/master.gfsgribfile.${PDY}${cyc}.f${fhour}
